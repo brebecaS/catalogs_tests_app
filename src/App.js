24dbdbc1a53fbf37efaafbb66f8d9230861678a7
brebecaS <birleanu0rebeca@gmail.com>
2024-07-30 14:57:32 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div class="wrapper">
+      <h1>Catalogs</h1>
+      <div class="container">
+        <h2>First Catalog</h2>
+        <div class="questions">
+          <section>
+            <div class="closed">
+              <h4>1. John</h4>
+            </div>
+          </section>
+          <section>
+            <div class="closed">
+              <h4>2. Job</h4>
+            </div>
+          </section>
+          <section>
+            <div class="closed">
+              <h4>3. Bob</h4>
+            </div>
+          </section>
+          <section>
+            <div class="closed">
+              <h4>4. Maria</h4>
+            </div>
+          </section>
+          <section>
+            <div class="closed">
+              <h4>5. Ioana</h4>
+            </div>
+          </section>
+        </div>
+      </div>
+      <div class="container">
+        <h2>Second Catalog</h2>
+        <div class="questions">
+          <section>
+            <div class="closed">
+              <h4>1. Rebeca</h4>
+            </div>
+          </section>
+          <section>
+            <div class="closed">
+              <h4>2. Dan</h4>
+            </div>
+          </section>
+          <section>
+            <div class="closed">
+              <h4>3. David</h4>
+            </div>
+          </section>
+          <section>
+            <div class="closed">
+              <h4>4. Sara</h4>
+            </div>
+          </section>
+          <section>
+            <div class="closed">
+              <h4>5. Iosif</h4>
+            </div>
+          </section>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
