8e42876bd75da6e34959cf6b42a502008d7ee237
brebecaS <birleanu0rebeca@gmail.com>
2024-07-30 15:50:35 +0300

    added catalog html

--- src/App.js
+++ src/App.js
@@ -2,63 +2,63 @@ import "./App.css";
 
 function App() {
   return (
-    <div class="wrapper">
+    <div className="wrapper">
       <h1>Catalogs</h1>
-      <div class="container">
+      <div className="container magenta">
         <h2>First Catalog</h2>
-        <div class="questions">
+        <div className="questions">
           <section>
-            <div class="closed">
+            <div className="closed">
               <h4>1. John</h4>
             </div>
           </section>
           <section>
-            <div class="closed">
+            <div className="closed">
               <h4>2. Job</h4>
             </div>
           </section>
           <section>
-            <div class="closed">
+            <div className="closed">
               <h4>3. Bob</h4>
             </div>
           </section>
           <section>
-            <div class="closed">
+            <div className="closed">
               <h4>4. Maria</h4>
             </div>
           </section>
           <section>
-            <div class="closed">
+            <div className="closed">
               <h4>5. Ioana</h4>
             </div>
           </section>
         </div>
       </div>
-      <div class="container">
+      <div className="container magenta">
         <h2>Second Catalog</h2>
-        <div class="questions">
+        <div className="questions">
           <section>
-            <div class="closed">
+            <div className="closed">
               <h4>1. Rebeca</h4>
             </div>
           </section>
           <section>
-            <div class="closed">
+            <div className="closed">
               <h4>2. Dan</h4>
             </div>
           </section>
           <section>
-            <div class="closed">
+            <div className="closed">
               <h4>3. David</h4>
             </div>
           </section>
           <section>
-            <div class="closed">
+            <div className="closed">
               <h4>4. Sara</h4>
             </div>
           </section>
           <section>
-            <div class="closed">
+            <div className="closed">
               <h4>5. Iosif</h4>
             </div>
           </section>
