55a41ff34a0ae7a28377826d1b3b1a3754a6efec
brebecaS <birleanu0rebeca@gmail.com>
2024-07-31 19:26:12 +0300

    test istorie

--- src/App.js
+++ src/App.js
@@ -7,58 +7,18 @@ function App() {
   const listaElevi = ["REbeca", "Andrei", "Ioan"];
   const listaElevi2 = ["Ana", "Ioana", "Ioan"];
   const questionList = ["2 + 5 = ?", "20 - 4 = ?", "-9 -10 = ?"];
+  const questionList2 = [
+    "n ce an a inceput primul razbui mondial ?",
+    "In ce an a avut loc marea unire ?",
+    "Cine e fost domnitorul cu cea mai lunga domnie ?",
+    "Intre ce ani a domnit stefan cel mare ?",
+    "In ce an a intrat romania in primul razboi mondial ?",
+  ];
 
   return (
     <div className="wrapper">
       <Titlu title={"Tests"} />
-      <div className="container blue">
-        <h2>Test de istorie</h2>
-        <div className="questions">
-          <section>
-            <div className="closed">
-              <h4>In ce an a inceput primul razbui mondial ?</h4>
-              <button>+</button>
-              <p className="hide">1914</p>
-            </div>
-          </section>
-        </div>
-        <div className="questions">
-          <section>
-            <div className="closed">
-              <h4>In ce an a avut loc marea unire ?</h4>
-              <button>+</button>
-              <p className="hide">1918</p>
-            </div>
-          </section>
-        </div>
-        <div className="questions">
-          <section>
-            <div className="closed">
-              <h4>Cine e fost domnitorul cu cea mai lunga domnie ?</h4>
-              <button>+</button>
-              <p className="hide">Stefan cel mare, 47 de ani</p>
-            </div>
-          </section>
-        </div>
-        <div className="questions">
-          <section>
-            <div className="closed">
-              <h4>Intre ce ani a domnit stefan cel mare ?</h4>
-              <button>+</button>
-              <p className="hide">intre anii 1457 și 1504</p>
-            </div>
-          </section>
-        </div>
-        <div className="questions">
-          <section>
-            <div className="closed">
-              <h4>In ce an a intrat romania in primul razboi mondial ?</h4>
-              <button>+</button>
-              <p className="hide">1916</p>
-            </div>
-          </section>
-        </div>
-      </div>
+      <Test title={"Test de Istorie"} questionList={questionList2} />
       <Test title={"Test de Matematica"} questionList={questionList} />
 
       <Titlu title={"Catalogs"} />
