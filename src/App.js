cecefd9be08283cf9d060ddbd776886097ce767a
brebecaS <birleanu0rebeca@gmail.com>
2024-07-31 18:31:57 +0300

    test component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Catalog from "./Catalog";
 import Titlu from "./Titlu";
+import Test from "./Test";
 
 function App() {
   const listaElevi = ["REbeca", "Andrei", "Ioan"];
@@ -57,36 +58,7 @@ function App() {
           </section>
         </div>
       </div>
-      <div className="container blue">
-        <h2>Test de Matematica</h2>
-        <div className="questions">
-          <section>
-            <div className="closed">
-              <h4>2 + 5 = ?</h4>
-              <button>+</button>
-              <p className="hide">7</p>
-            </div>
-          </section>
-        </div>
-        <div className="questions">
-          <section>
-            <div className="closed">
-              <h4>20 - 4 = ?</h4>
-              <button>+</button>
-              <p className="hide">16</p>
-            </div>
-          </section>
-        </div>
-        <div className="questions">
-          <section>
-            <div className="closed">
-              <h4>-9 -10 = ?</h4>
-              <button>+</button>
-              <p className="hide">-19</p>
-            </div>
-          </section>
-        </div>
-      </div>
+      <Test />
 
       <Titlu title={"Catalogs"} />
       <Catalog title={"First Catalog"} listaElevi={listaElevi} />
