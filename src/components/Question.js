8c74b2344478c6476a408aca2e585153484ecde5
brebecaS <birleanu0rebeca@gmail.com>
2024-08-07 19:40:27 +0300

    implemented hide and show answer

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,19 +1,32 @@
+import { useState } from "react";
+
 const Question = ({ questionObject }) => {
   const styleObj = {
     display: "block",
   };
+  const [showAnswer, setShowAnswer] = useState(false);
   return (
     <div className="questions" key={questionObject.question}>
       <section>
         <div className="closed" style={styleObj}>
           <div>
             <h4>{questionObject.question}</h4>
-            <button>+</button>
+            <button
+              onClick={() => {
+                if (showAnswer === true) setShowAnswer(false);
+                else setShowAnswer(true);
+                // setShowAnswer(!showAnswer)
+              }}
+            >
+              {showAnswer === true ? "-" : "+"}
+            </button>
           </div>
 
-          <p className="hide">
-            <b>Raspuns:</b> {questionObject.response}
-          </p>
+          {showAnswer === true && (
+            <p>
+              <b>Raspuns:</b> {questionObject.response}
+            </p>
+          )}
         </div>
       </section>
     </div>
