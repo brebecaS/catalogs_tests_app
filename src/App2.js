5638e303d800719a24975eb82d3983f771f64fbc
brebecaS <birleanu0rebeca@gmail.com>
2024-10-08 20:04:47 +0300

    test istorie structura

--- src/App2.js
+++ src/App2.js
@@ -17,6 +17,37 @@ const App2 = () => {
         <Question questionText={"-0 -10 = ?"} />
       </div>
 
+      <div className="container blue">
+        <h2>Test de Istorie</h2>
+        <div className="questions">
+          <section>
+            <div className="closed">
+              <h4>In ce an a inceput primul razbui mondial ?</h4>
+              <button>+</button>
+              <p className="hide">7</p>
+            </div>
+          </section>
+        </div>
+        <div className="questions">
+          <section>
+            <div className="closed">
+              <h4>In ce an a avut loc marea unire ?</h4>
+              <button>+</button>
+              <p className="hide">7</p>
+            </div>
+          </section>
+        </div>
+        <div className="questions">
+          <section>
+            <div className="closed">
+              <h4>Cine e fost domnitorul cu cea mai lunga domnie ?</h4>
+              <button>+</button>
+              <p className="hide">7</p>
+            </div>
+          </section>
+        </div>
+      </div>
+
       <h1>Catalogs</h1>
 
       <Catalog studentsArray={studentsList1} title={"First Catalog"} />
