c4e87b63871a7198a04cbcda5e755c1d688bb129
brebecaS <birleanu0rebeca@gmail.com>
2024-10-08 19:12:25 +0300

    catalog component

--- src/App2.js
+++ src/App2.js
@@ -1,38 +1,17 @@
 import "./App.css";
-import Section from "./Section";
+import Catalog from "./Catalog";
 
 const App2 = () => {
   const studentsList1 = ["Clau", "Cata M", "Simo", "Tudor", "George"];
-  const studentsElements = studentsList1.map((element, index) => {
-    return (
-      <Section
-        number={index + 1}
-        student={element}
-      />
-    );
-  });
-
   const studentsList2 = ["Beca", "A Stefan", "Cata", "Mircea", "Cata I"];
-  const studentsElements2 = studentsList2.map((element, index) => {
-    return (
-      <Section
-        number={index + 1}
-        student={element}
-      />
-    );
-  });
 
   return (
     <div className="wrapper">
       <h1>Catalogs</h1>
-      <div className="container magenta">
-        <h2>First Catalog</h2>
-        <div className="questions">{studentsElements}</div>
-      </div>
-      <div className="container magenta">
-        <h2>Second Catalog</h2>
-        <div className="questions">{studentsElements2}</div>
-      </div>
+
+      <Catalog studentsArray={studentsList1} />
+
+      <Catalog studentsArray={studentsList2} />
     </div>
   );
 };
---
+++ src/Catalog.js
@@ -0,0 +1,16 @@
+import Section from "./Section";
+
+const Catalog = (props) => {
+  const studentsElements = props.studentsArray.map((element, index) => (
+    <Section number={index + 1} student={element} />
+  ));
+
+  return (
+    <div className="container magenta">
+      <h2>First Catalog</h2>
+      <div className="questions">{studentsElements}</div>
+    </div>
+  );
+};
+
+export default Catalog;
