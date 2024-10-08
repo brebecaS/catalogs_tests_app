83a3447790a783907cf37086a02116739fc39e76
brebecaS <birleanu0rebeca@gmail.com>
2024-10-08 20:27:12 +0300

    use map

--- src/App2.js
+++ src/App2.js
@@ -5,38 +5,39 @@ import Question from "./Question";
 const App2 = () => {
   const studentsList1 = ["Clau", "Cata M", "Simo", "Tudor", "George"];
   const studentsList2 = ["Beca", "A Stefan", "Cata", "Mircea", "Cata I"];
+  const mathQuestions = ["2 + 5 = ?", "20 - 4 = ?", "-9 -10 = ?", "-0 -10 = ?"];
+  const historyQuestions = [
+    "In ce an a inceput primul razboi mondial ?",
+    "In ce an a avut loc marea unire ?",
+    "Cine e fost domnitorul cu cea mai lunga domnie ?",
+  ];
+
+  const mathQuestionElements = mathQuestions.map((element) => {
+    return <Question key={element} questionText={element} />;
+  });
+
+  const historyQuestionElements = historyQuestions.map((element) => {
+    return <Question key={element} questionText={element} />;
+  });
 
   return (
     <div className="wrapper">
       <h1>Teste</h1>
       <div className="container blue">
         <h2>Test de Matematica</h2>
-        <Question questionText={"2 + 5 = ?"} />
-        <Question questionText={"20 - 4 = ?"} />
-        <Question questionText={"-9 -10 = ?"} />
-        <Question questionText={"-0 -10 = ?"} />
+        {mathQuestionElements}
       </div>
 
       <div className="container blue">
         <h2>Test de Istorie</h2>
-        <Question questionText={"In ce an a inceput primul razboi mondial ?"} />
-        <Question questionText={"In ce an a avut loc marea unire ?"} />
-        <Question
-          questionText={"Cine e fost domnitorul cu cea mai lunga domnie ?"}
-        />
+        {historyQuestionElements}
       </div>
 
       <h1>Catalogs</h1>
 
-      <Catalog
-        studentsArray={studentsList1}
-        title={"First Catalog"}
-      />
+      <Catalog studentsArray={studentsList1} title={"First Catalog"} />
 
-      <Catalog
-        studentsArray={studentsList2}
-        title={"Second Catalog"}
-      />
+      <Catalog studentsArray={studentsList2} title={"Second Catalog"} />
     </div>
   );
 };
