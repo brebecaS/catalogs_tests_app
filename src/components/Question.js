2c8f8cb3a35c9ee3dc62c801eaf12054dad6ce8f
brebecaS <birleanu0rebeca@gmail.com>
2024-10-10 20:41:03 +0300

    useState

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,30 +1,35 @@
+import { useState } from "react";
+
 const Question = ({ index, questionText, answer }) => {
-  const blackBackgroundStyle = {
-    backgroundColor: index === 0 ? "black" : "white",
-    color: index === 0 ? "white" : "black",
-  };
-  // let blackBackgroundStyle;
-  // if (index === 0) {
-  //   blackBackgroundStyle = { backgroundColor: "black" };
-  // } else blackBackgroundStyle = { backgroundColor: "white" };
+  const [isHidden, setIsHidden] = useState(true); // isHidden =false
+  const [buttonSign, setButtonSign] = useState("+");
+
+  function handleClick() {
+    if (isHidden === true) {
+      setIsHidden(false);
+      setButtonSign("-");
+    } else {
+      setIsHidden(true);
+      setButtonSign("+");
+    }
+    // setIsHidden(!isHidden); //true =>  !true => false
+    // false => !false => true
+    // setIsHidden((previousValue) => !previousValue);
+  }
 
   return (
-    <div
-      className="questions"
-      style={{
-        fontSize: 19, // font-size: 19px;
-        padding: 10,
-        border: "1px solid black",
-      }}
-    >
+    <div className="questions">
       <section>
-        <div
-          // style={blackBackgroundStyle}
-          className="closed"
-        >
-          <h4>{questionText}</h4>
-          <button>+</button>
-          <p>{answer}</p>
+        <div className="closed" style={{ display: "block" }}>
+          <div>
+            <h4>{questionText}</h4>
+            <button onClick={handleClick}>{buttonSign}</button>
+          </div>
+
+          <p className={isHidden === true ? "hide" : ""}>
+            Raspuns:
+            {answer}
+          </p>
         </div>
       </section>
     </div>
