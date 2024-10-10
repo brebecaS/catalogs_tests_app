19295270f0d8170b36367f9891c8e53e78653940
brebecaS <birleanu0rebeca@gmail.com>
2024-10-10 18:08:28 +0300

    comment

--- src/components/Test.js
+++ src/components/Test.js
@@ -5,7 +5,7 @@ const Test = ({ questionsList, title }) => {
   const mathQuestionElements = questionsList.map((element, index) => {
     return <Question key={element} questionText={element} index={index} />;
   });
-
+  // some comment
   return (
     <div className="container blue">
       <h2>{title}</h2>
