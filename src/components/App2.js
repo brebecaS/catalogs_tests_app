893a05e7f7380f632784eab4b611736525797ed7
evLRiseAlex <alexandrei_stef@yahoo.com>
2024-10-21 19:12:08 +0300

    implemented nav buttons

--- src/components/App2.js
+++ src/components/App2.js
@@ -7,33 +7,43 @@ import {
   mathQuestions,
   historyQuestions,
 } from "../constants";
+import { useState } from "react";
 
 const App2 = () => {
+  const [showTable, setShowTable] = useState(`tests`);
   return (
     <div className="wrapper">
       <div className={"tab"}>
         <button
           className={"tablinks"}
-          // onClick={}
+          onClick={() => {
+            setShowTable(`tests`);
+          }}
         >
           Tests
         </button>
         <button
           className={"tablinks"}
-          // onClick={}
+          onClick={() => {
+            setShowTable(`catalogs`);
+          }}
         >
           Catalogs
         </button>
       </div>
-      <h1>Teste</h1>
+      <div className={showTable === `tests` ? `` : `hide`}>
+        <h1>Teste</h1>
 
-      <Test questionsList={mathQuestions} title={"Math Test"} />
-      <Test questionsList={historyQuestions} title={"History Test"} />
+        <Test questionsList={mathQuestions} title={"Math Test"} />
+        <Test questionsList={historyQuestions} title={"History Test"} />
+      </div>
 
-      <h1>Catalogs</h1>
+      <div className={showTable === `catalogs` ? `` : `hide`}>
+        <h1>Catalogs</h1>
 
-      <Catalog studentsArray={students1} title={"First Catalog"} />
-      <Catalog studentsArray={students2} title={"Second Catalog"} />
+        <Catalog studentsArray={students1} title={"First Catalog"} />
+        <Catalog studentsArray={students2} title={"Second Catalog"} />
+      </div>
     </div>
   );
 };
